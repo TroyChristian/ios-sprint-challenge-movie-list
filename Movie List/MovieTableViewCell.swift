aae7594ab50358c0e291648e96fb3ebeb4cbd1de
Troy Christian <apostasy8@gmail.com>
2019-11-08 13:23:02 -0500

    AddNewMovieViewController in place

---
+++ Movie List/MovieTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_219 on 11/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieName: UILabel!
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBAction func onButtonTapped(_ sender: Any) {
+    }
+    
+}
