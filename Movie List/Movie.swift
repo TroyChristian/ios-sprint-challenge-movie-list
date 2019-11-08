6ef486922566562875244e9d0b9391f61efb3223
Troy Christian <apostasy8@gmail.com>
2019-11-08 11:35:01 -0500

    Nav Controller, TableView, Bar Item Button, made in StoryBoard.

---
+++ Movie List/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_219 on 11/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+struct Movie {
+    var name:String
+    var seen: Bool = false
+}
